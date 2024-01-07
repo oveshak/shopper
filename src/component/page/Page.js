c5255c756d7805e3ea1b0a6c0dc56c327b90278a
oveshak <oveshaktoton999@gmail.com>
2024-01-07 21:14:37 +0600

    add all file

--- src/component/page/Page.js
+++ src/component/page/Page.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Shop from '../home/shop/Shop';
 import Footer from '../common/footer/Footer';
 import Shopcategory from '../home/shoprcategory/Shopcategory';
+import Product from '../home/product/Product';
+import Cart from '../home/cart/Cart';
+import LoginSignup from '../home/loginsignup/LoginSignup';
 const Page = () => {
   return (
     <>
@@ -11,9 +14,14 @@ const Page = () => {
     <Header/>
     <Routes>
       <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<Shopcategory category="men"/>}/>
+      <Route path='/men' element={<Shopcategory category="mens"/>}/>
       <Route path='/womens' element={<Shopcategory category="women"/>}/>
       <Route path='/kids' element={<Shopcategory category="kid"/>}/>
+      <Route path='product' element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
     </Routes>
     <Footer/>
     </BrowserRouter>
